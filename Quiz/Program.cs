459187005761486dc58c2b864cc73ef859e7ad52
JanJanusz <60319333+JanJanusz2000@users.noreply.github.com>
2021-10-31 18:32:02 +0100

    super zmiany z pierwszej lekcji
    haha

---
+++ Quiz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Pytanie pytanie1= new Pytanie();
+            pytanie1.tresc = "Jak miał na imie Einstein?";
+            Pytanie pytanie2 = new Pytanie();
+            Pytanie pytanie3 = new Pytanie();
+            Pytanie pytanie4 = new Pytanie();
+
+            Console.WriteLine("he");
+        }
+    }
+}
---
+++ Quiz/Pytanie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Pytanie
+    {
+        public int id { get; set; }
+        public int Kategoria { get; set; }
+        public string tresc { get; set; }
+        public string odp1 { get; set; }
+        public string odp2 { get; set; }
+        public string odp3 { get; set; }
+        public string odp4 { get; set; }
+
+        public int Dodaj(int x, int y)
+        {
+            return x+y;
+        }
+    }
+}
